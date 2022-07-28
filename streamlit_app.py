68ba0d423cedd8bebb1f853c6b4287334ebdbe24
LauraSPelegrina <110201913+lauraspelegrina@users.noreply.github.com>
2022-07-28 19:47:32 -0300

    Update streamlit_app.py
    title and header changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit 
 import pandas
 
-streamlit.title("My Parents New Healthy Dinner")
-streamlit.header("Breakfast Menu")
+streamlit.title("My Mom's New Healthy Dinner")
+streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")

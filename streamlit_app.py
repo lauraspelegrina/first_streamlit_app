5a7feef553c17535148fe5cd2c2ca0e0d9a30766
LauraSPelegrina <110201913+lauraspelegrina@users.noreply.github.com>
2022-08-05 18:22:46 -0300

    importing request biblio
    importing request biblio

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit 
 import pandas
+import requests
 
 streamlit.title("My Mom's New Healthy Dinner")
 streamlit.header("Breakfast Favorites")
@@ -15,3 +16,6 @@ fruits_selected = streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
